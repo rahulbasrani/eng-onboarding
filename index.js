9552855fe74c866fcb1be89656fb04d1c95251dd
ravi-basrani <rahulbasranikhairthal@gmail.com>
2021-02-01 13:30:32 +0530

    Format the Code & Raise a PR

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-var a,b,result;
-		a=0;
-		b=1;
-		result=b;
-		for(var i=1;i<100;i++){
-			console.log(result);
-			result=a+b;
-			a=b;
-            b=result;
-        }
-
-        console.log(result);
+var a, b, result; a = 0;
+b = 1;
+result = b;
+var b = "jason";
+for (var i = 1; i < 100; i++) {
+	console.log(result);
+	result = a + b;
+	a = b;
+	b = result;
+}
