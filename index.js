b42d4c66bf5ec8081142ce3b52d4e127dc94b913
ravi-basrani <rahulbasranikhairthal@gmail.com>
2021-02-02 11:37:53 +0530

    Use 2 spacing instead of 4

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let a, b, result; a = 0;
 b = 1;
 result = b;
 for (let i = 1; i < 10; i++) {
-	console.log(result);
-	result = a + b;
-	a = b;
-	b = result;
+  console.log(result);
+  result = a + b;
+  a = b;
+  b = result;
 }
